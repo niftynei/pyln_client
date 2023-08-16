250b8a237138cd0d9a745fb1d897e5491dcb7ff4
niftynei <niftynei@gmail.com>
2023-08-16 11:15:17 -0500

    add pyln. to package name

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
+from setuptools import setup
 
 packages = \
-['client']
-
-package_dir={"":"pyln"}
+['pyln.client']
 
 package_data = \
 {'': ['*']}
@@ -22,7 +20,7 @@ setup_kwargs = {
     'maintainer': 'None',
     'maintainer_email': 'None',
     'url': 'None',
-    'packages': find_packages(),
+    'packages': packages,
     'package_data': package_data,
     'install_requires': install_requires,
     'python_requires': '>=3.7,<4.0',
